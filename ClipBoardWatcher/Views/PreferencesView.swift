cbb527e639bb9af0a0a36cbf937a6a432610a977
RogueFiend <thanmayprebeesh@gmail.com>
2026-08-24 19:45:20 +0530

    Changed package depencies from HotKeys to KeyboardShortcuts, changed how menubar works, added a settings view and a quit button(finally).

---
+++ ClipBoardWatcher/Views/PreferencesView.swift
@@ -0,0 +1,14 @@
+//
+//  PreferencesView.swift
+//  ClipBoardWatcher
+//
+//  Created by Thanmay Prebeesh on 24/08/2026.
+//
+
+import SwiftUI
+
+struct PreferencesView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
